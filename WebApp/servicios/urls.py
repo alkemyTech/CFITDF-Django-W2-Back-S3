63dad627c75fa4771be3b030e8f11ee027267a20
bambamrg <ivan_gabriel_aranda@hotmail.com>
2025-05-20 17:38:26 -0300

    primer commit de empleado

---
+++ WebApp/servicios/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = 'WebApp'
+
+urlpatterns = [
+    #path('', views.home, name='home'),
+
+    # Empleado
+    path('empleado/crear/', views.EmpleadoCreateView.as_view(), name='empleado_crear'),
+    path('empleado/<int:pk>/', views.EmpleadoDetailView.as_view(), name='empleado_detalle'),
+    path('empleado/', views.EmpleadoListView.as_view(), name='empleado_listar'),
+    path('empleado/listar/', views.EmpleadoListView.as_view(), name='empleado_listar'),
+    path('empleado/listar/inactivos/', views.EmpleadoListView.as_view(), {'inactivos': True}, name='empleado_listar_inactivos'),
+    path('empleado/editar/<int:pk>/', views.EmpleadoUpdateView.as_view(), name='empleado_editar'),
+    path('empleado/cambiar_estado/<int:pk>/', views.Empleado_cambiar_estado, name='empleado_cambiar_estado'),
+]
--- WebApp/servicios/views.py
+++ WebApp/servicios/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Empleado
 
 # Create your views here.
+
+# Empleado
+class EmpleadoCreateView(CreateView):
+    model = Empleado
+    template_name = 'editar.html'
+    fields = '__all__'
+    success_url = reverse_lazy('WebApp:empleado_listar')
+
+    # Esto permite personalizar el contexto que se pasa a la plantilla.
+    # De esta manera, editar.html puede ser usado tanto para crear como
+    # para editar, dependiendo de la vista que lo llame.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["titulo_vista"] = "Crear Empleado"
+        return context
+    
+
+class EmpleadoDetailView(DetailView):
+    model = Empleado
+    template_name = 'empleado/detalle.html'
+    context_object_name = 'empleado'
+
+
+class EmpleadoListView(ListView):
+    model = Empleado
+    template_name = 'empleado/listar.html'
+    context_object_name = 'listado_empleado'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["listando_inactivos"] = self.kwargs.get('inactivos', False)
+        return context
+    
+    def get_queryset(self):
+        # Filtra empleados activos por defecto, pero permite listar inactivos
+        # si se pasa 'inactivos' en la URL
+        queryset = super().get_queryset()
+        if self.kwargs.get('inactivos', False):
+            return queryset.filter(activo=False)
+        return queryset.filter(activo=True)
+    
+
+class EmpleadoUpdateView(UpdateView):
+    model = Empleado
+    template_name = 'editar.html'
+    fields = '__all__'
+    success_url = reverse_lazy('WebApp:empleado_listar')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["titulo_vista"] = "Modificar Empleado"
+        return context
+    
+
+def empleado_cambiar_estado(request, pk):
+    empleado = get_object_or_404(Empleado, id=pk)
+    empleado.activo = not empleado.activo
+    empleado.save()
+    return HttpResponseRedirect(reverse_lazy('WebApp:empleado_listar'))
