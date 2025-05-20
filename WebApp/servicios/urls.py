8ef54d5687166c8676e66daebd68feefd558ffc5
Luxiano <luxiano.a2000@gmail.com>
2025-05-20 14:31:51 -0300

    CRUD de Servicios. Templates genéricos para eliminar y crear/editar. Listado de activos, inactivos. Función para cambiar estado activo/inactivo. Modificación de urls.py para agregar servicios/urls.py; y de settings.py para integrar carpeta templates/.

---
+++ WebApp/servicios/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = 'WebApp'
+
+urlpatterns = [
+    #path('', views.home, name='home'),
+
+    # Servicios
+    path('servicios/crear/', views.ServicioCreateView.as_view(), name='servicio_crear'),
+    path('servicios/<int:pk>/', views.ServicioDetailView.as_view(), name='servicio_detalle'),
+    path('servicios/', views.ServicioListView.as_view(), name='servicio_listar'),
+    path('servicios/listar/', views.ServicioListView.as_view(), name='servicio_listar'),
+    path('servicios/listar/inactivos/', views.ServicioListView.as_view(), {'inactivos': True}, name='servicio_listar_inactivos'),
+    path('servicios/editar/<int:pk>/', views.ServicioUpdateView.as_view(), name='servicio_editar'),
+    path('servicios/cambiar_estado/<int:pk>/', views.servicio_cambiar_estado, name='servicio_cambiar_estado'),
+    path('servicios/eliminar/<int:pk>/', views.ServicioDeleteView.as_view(), name='servicio_eliminar'),
+]
--- WebApp/servicios/views.py
+++ WebApp/servicios/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Servicio
 
 # Create your views here.
+
+# Servicios
+class ServicioCreateView(CreateView):
+    model = Servicio
+    template_name = 'editar.html'
+    fields = '__all__'
+    success_url = reverse_lazy('WebApp:servicio_listar')
+
+    # Esto permite personalizar el contexto que se pasa a la plantilla.
+    # De esta manera, editar.html puede ser usado tanto para crear como
+    # para editar, dependiendo de la vista que lo llame.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["titulo_vista"] = "Crear Servicio"
+        return context
+    
+
+class ServicioDetailView(DetailView):
+    model = Servicio
+    template_name = 'servicios/detalle.html'
+    context_object_name = 'servicio'
+
+
+class ServicioListView(ListView):
+    model = Servicio
+    template_name = 'servicios/listar.html'
+    context_object_name = 'listado_servicios'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["listando_inactivos"] = self.kwargs.get('inactivos', False)
+        return context
+    
+    def get_queryset(self):
+        # Filtra servicios activos por defecto, pero permite listar inactivos
+        # si se pasa 'inactivos' en la URL
+        queryset = super().get_queryset()
+        if self.kwargs.get('inactivos', False):
+            return queryset.filter(activo=False)
+        return queryset.filter(activo=True)
+    
+
+class ServicioUpdateView(UpdateView):
+    model = Servicio
+    template_name = 'editar.html'
+    fields = '__all__'
+    success_url = reverse_lazy('WebApp:servicio_listar')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["titulo_vista"] = "Modificar Servicio"
+        return context
+    
+
+class ServicioDeleteView(DeleteView):
+    model = Servicio
+    template_name = 'eliminar.html'
+    context_object_name = 'servicio'
+    success_url = reverse_lazy('WebApp:servicio_listar')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["titulo_vista"] = "Eliminar Servicio"
+        return context
+    
+
+def servicio_cambiar_estado(request, pk):
+    servicio = get_object_or_404(Servicio, id=pk)
+    servicio.activo = not servicio.activo
+    servicio.save()
+    return HttpResponseRedirect(reverse_lazy('WebApp:servicio_listar'))
+
+# Clientes
