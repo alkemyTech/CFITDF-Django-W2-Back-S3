7ee15a7ffecab4e3dbc5792b2e5f1c51116269e0
Luxiano <luxiano.a2000@gmail.com>
2025-05-21 16:24:15 -0300

    Vistas de detalle y listado de servicios y sus correspondientes urls

--- WebApp/servicios/urls.py
+++ WebApp/servicios/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
 
     # Servicios
     path('servicios/crear/', views.ServicioCreateView.as_view(), name='servicio_crear'),
+    path('servicios/<int:pk>/', views.ServicioDetailView.as_view(), name='servicio_detalle'),
+    path('servicios/', views.ServicioListView.as_view(), name='servicio_listar'),
+    path('servicios/listar/', views.ServicioListView.as_view(), name='servicio_listar'),
+    path('servicios/listar/inactivos/', views.ServicioListView.as_view(), {'inactivos': True}, name='servicio_listar_inactivos'),
 ]
--- WebApp/servicios/views.py
+++ WebApp/servicios/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView
 from django.urls import reverse_lazy
 from .models import Servicio
 
@@ -9,7 +9,7 @@ class ServicioCreateView(CreateView):
     model = Servicio
     template_name = 'editar.html'
     fields = '__all__'
-    success_url = reverse_lazy('WebApp:servicio_crear')
+    success_url = reverse_lazy('WebApp:servicio_listar')
 
     # Esto permite personalizar el contexto que se pasa a la plantilla.
     # De esta manera, editar.html puede ser usado tanto para crear como
@@ -18,3 +18,28 @@ class ServicioCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context["titulo_vista"] = "Crear Servicio"
         return context
+
+
+class ServicioDetailView(DetailView):
+    model = Servicio
+    template_name = 'servicios/detalle.html'
+    context_object_name = 'servicio'
+
+
+class ServicioListView(ListView):
+    model = Servicio
+    template_name = 'servicios/listar.html'
+    context_object_name = 'listado_servicios'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["listando_inactivos"] = self.kwargs.get('inactivos', False)
+        return context
+    
+    def get_queryset(self):
+        # Filtra servicios activos por defecto, pero permite listar inactivos
+        # si se pasa 'inactivos' en la URL
+        queryset = super().get_queryset()
+        if self.kwargs.get('inactivos', False):
+            return queryset.filter(activo=False)
+        return queryset.filter(activo=True)
