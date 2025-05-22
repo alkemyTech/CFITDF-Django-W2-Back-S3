fc4b1fc85d878512515ba2fa2d4b03ba68823ee5
martin-vilchez <155126905+martin-vilchez@users.noreply.github.com>
2025-05-21 23:29:32 -0300

    READ para modelo coordinador (detail y list views)

--- WebApp/servicios/urls.py
+++ WebApp/servicios/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     # Servicios
     path('servicios/crear/', views.ServicioCreateView.as_view(), name='servicio_crear'),
     #Coordinador
-    path('coordinador/crear/', views.CoordinadorCreateView.as_view(), name='coordinador_crear')
+    path('coordinador/crear/', views.CoordinadorCreateView.as_view(), name='coordinador_crear'),
+    path('coordinador/<int:pk>/', views.CoordinadorDetailView.as_view(), name = 'coordinador_detalle'),
+    path('coordinador/', views.CoordinadorListView.as_view(), name = 'coordinador_listar'),
+    path('coordinador/inactivos', views.CoordinadorListView.as_view(), {'inactivos':True}, name = 'coordinador_listar_inactivos'),
 
 ]
--- WebApp/servicios/views.py
+++ WebApp/servicios/views.py
@@ -31,3 +31,26 @@ class CoordinadorCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context["titulo_vista"] = "Crear Coordinador"
         return context
+    
+
+class CoordinadorDetailView(DetailView):
+    model = Coordinador
+    template_name = 'coordinador/detalle.html'
+    context_object_name = 'coordinador'
+
+
+class CoordinadorListView(ListView):
+    model = Coordinador
+    template_name = 'coordinador/listar.html'
+    context_object_name = 'listado_coordinador'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["listando_inactivo"] = self.kwargs.get('inactivos',False)
+        return context
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.kwargs.get('inactivos', False):
+            return queryset.filter(activo=False)
+        return queryset.filter(activo=True)
