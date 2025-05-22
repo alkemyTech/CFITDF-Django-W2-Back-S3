f83dfe3a07c8b0a848b96f447001049993c69fd5
VictorVisnuk <victorvisnuk@gmail.com>
2025-05-22 12:43:29 -0300

    merge-coordinador-read

--- WebApp/servicios/views.py
+++ WebApp/servicios/views.py
@@ -60,6 +60,28 @@ class CoordinadorCreateView(CreateView):
         context["titulo_vista"] = "Crear Coordinador"
         return context
     
+class CoordinadorDetailView(DetailView):
+    model = Coordinador
+    template_name = 'coordinador/detalle.html'
+    context_object_name = 'coordinador'
+
+
+class CoordinadorListView(ListView):
+    model = Coordinador
+    template_name = 'coordinador/listar.html'
+    context_object_name = 'listado_coordinador'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["listando_inactivo"] = self.kwargs.get('inactivos',False)
+        return context
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.kwargs.get('inactivos', False):
+            return queryset.filter(activo=False)
+        return queryset.filter(activo=True)
+    
     
 # ReservaServicio
 class ReservaServicioCreateView(CreateView):
@@ -92,3 +114,5 @@ class ReservaServicioListView(ListView):
     
     def get_queryset(self):
         return ReservaServicio.objects.all() 
+    
+
