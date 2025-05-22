09311e2cdf35d7c4f0accd75e98f9e5616dcfcbf
VictorVisnuk <victorvisnuk@gmail.com>
2025-05-22 12:43:49 -0300

    typo

--- WebApp/servicios/views.py
+++ WebApp/servicios/views.py
@@ -73,7 +73,7 @@ class CoordinadorListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["listando_inactivo"] = self.kwargs.get('inactivos',False)
+        context["listando_inactivos"] = self.kwargs.get('inactivos',False)
         return context
     
     def get_queryset(self):
