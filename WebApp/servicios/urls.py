128b3a249d32db4e690c53b568a18769b259c505
Luxiano <luxiano.a2000@gmail.com>
2025-05-28 12:14:51 -0300

    Corrección en el path empleado/listar/inactivos/

--- WebApp/servicios/urls.py
+++ WebApp/servicios/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('empleado/crear/', views.EmpleadoCreateView.as_view(), name='empleado_crear'),
     path('empleado/<int:pk>/', views.EmpleadoDetailView.as_view(), name='empleado_detalle'),
     path('empleado/listar/', views.EmpleadoListView.as_view(), name='empleado_listar'),
-    path('empleado/listar/inactivos/', views.EmpleadoListView.as_view(), name='empleado_listar_inactivos'),
+    path('empleado/listar/inactivos/', views.EmpleadoListView.as_view(), {'inactivos': True}, name='empleado_listar_inactivos'),
     path('empleado/editar/<int:pk>/', views.EmpleadoUpdateView.as_view(), name='empleado_editar'),
     path('empleado/cambiar_estado/<int:pk>/', views.empleado_cambiar_estado, name='empleado_cambiar_estado'),
     # Cliente
