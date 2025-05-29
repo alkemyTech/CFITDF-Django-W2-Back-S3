be893b9004cb977db73427970630078bde608a74
Luxiano <luxiano.a2000@gmail.com>
2025-05-29 12:01:28 -0300

    API: Creación de vistas List y Retrieve de Servicios, con titulo personalizado; creación api/urls.py con paths para listar y detalle de servicios; agregado del path de la API en urls.py del proyecto.

--- WebApp/api/views.py
+++ WebApp/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .serializers import ServicioSerializer
+from servicios.models import Servicio
 
 # Create your views here.
+class ServicioListAPIView(ListAPIView):
+    queryset = Servicio.objects.all()
+    serializer_class = ServicioSerializer
+
+    def get_view_name(self):
+        return "Lista de Servicios"
+
+
+class ServicioRetrieveAPIView(RetrieveAPIView):
+    queryset = Servicio.objects.all()
+    serializer_class = ServicioSerializer
+
+    def get_view_name(self):
+        return "Detalle de Servicio"
